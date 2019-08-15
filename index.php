7e2cc5ffc98c6fb2cfb7f41969e1b0aa2c42a02c
Julio C Becker <julio_becker@estudante.sc.senai.br>
2019-08-15 16:19:36 -0300

    Ajax envio de cadastro close #14

--- index.php
+++ index.php
@@ -154,9 +154,37 @@
                 }
             });
             //Formulário de Cadastro de usuário
-            $('#btnRegistrar').click(function(e) {});
+            $('#btnRegistrar').click(function(e) {
+                let formLogin = document.querySelector("#formCadastro");
+                if (formLogin.checkValidity()) {
+                    e.preventDefault(); //Não recarregar a página
+                    $.ajax({
+                        url: 'recebe.php',
+                        method: 'post',
+                        data: $('#formCadastro').serialize() + '&action=cadastro',
+                        success: function(resposta) {
+                            $('#alerta').show();
+                            $('#resultado').html("resposta:" + resposta);
+                        }
+                    });
+                }
+            });
             //Formulário para mudar de senha
-            $('#btnEnviarEmail').click(function(e) {});
+            $('#btnEnviarEmail').click(function(e) {
+                let formLogin = document.querySelector("#formSenha");
+                if (formLogin.checkValidity()) {
+                    e.preventDefault(); //Não recarregar a página
+                    $.ajax({
+                        url: 'recebe.php',
+                        method: 'post',
+                        data: $('#formSenha').serialize() + '&action=senha',
+                        success: function(resposta) {
+                            $('#alerta').show();
+                            $('#resultado').html("resposta:" + resposta);
+                        }
+                    });
+                }
+            });
             //Trocar da Tela de Login para Recuperar Senha
             $("#btnEsqueci").click(function() {
                 $("#caixaLogin").hide();
