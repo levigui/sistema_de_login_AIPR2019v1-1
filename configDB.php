16425bbb9f893d651a76bdebeb14d2e84bfde65f
Julio C Becker <julio_becker@estudante.sc.senai.br>
2019-08-23 15:08:38 -0300

    Conexão com o banco de dados

---
+++ configDB.php
@@ -0,0 +1,16 @@
+<?php
+//configDb.php
+
+//Dados para escolha do DataBase (DB)
+$dbhost ="localhost";
+$dbuser = "root"; //Usuário Raíz (Rute)
+$dbpass = "";
+$dbname = "sistemaDeLogin";
+
+//conexão com o banco de dados
+$conecta = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+if($conecta->connect_error){
+    die("Não foi possível conectar ao Banco de Dados: " . $conecta->connect_error);
+}else{
+    //echo "<h1>Conectou no BD Manowwww!</h1>";
+}
