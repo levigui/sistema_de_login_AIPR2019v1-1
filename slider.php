10ca7afa5d5da6bed9e4b1ca1c6edadc29d401c5
Julio Cesar Becker <julio_becker@estudante.sc.senai>
2019-09-12 15:44:12 -0300

    Criação do slider.php
    Carrosel de Imagens

--- slider.php
+++ slider.php
@@ -13,10 +13,10 @@ require_once 'session.php';
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Perfil de <?= $usuario ?></title>
+    <title>Álbum de Fotos</title>
 </head>
 
-<body class="bg-dark">
+<body class="bg-danger">
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">Becker Systems</a>
@@ -26,20 +26,20 @@ require_once 'session.php';
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="profile.php">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="">link 1</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">link 2</a>
+                    <a class="nav-link" href="">link 2</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <?= $usuario ?>
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="slider.php">Fotos</a>
+                        <a class="dropdown-item" href="#">Fotos</a>
                         <a class="dropdown-item" href="#">Configurações</a>
                         <a class="dropdown-item" href="sair.php">Sair</a>
                     </div>
@@ -48,21 +48,48 @@ require_once 'session.php';
         </div>
     </nav>
 
-    <div class="container">
-        <div class="row">
-
-            <div class="col-9">
-                <h1 class="text-light mb-5 mt-4">Perfil do <?= $nome ?></h1>
+    <div class="bd-example border border-light" style="width: 1000px; display:block; margin:auto; margin-top:50px; margin-bottom:50px;">
+        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="imagens/yasuo.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Yasuo</h5>
+                        <p>" A morte é como o vento, está sempre ao meu lado "</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="imagens/draven.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Ezreal</h5>
+                        <p>" Seu lugar é no museu "</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="imagens/leesin.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Lee Sin</h5>
+                        <p>" Sua vontade, Minhas mãos "</p>
+                    </div>
+                </div>
             </div>
-            <div class="col-4"> <img src="<?= $imagem ?>" alt="Ícone" width="200" height="200"></div>
-
-            <div class="col-6">
-                <h2 class="text-light">Nome: <?= $nome ?></h2>
-                <h2 class="text-light">E-mail: <?= $email ?></h2>
-                <h2 class="text-light">Data de Cadastro: <?= date('d/m/Y H:i:s', strtotime($dataCriacao)) ?></h2>
-            </div>
+            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
     </div>
+
+
 
 
     <!-- Optional JavaScript -->
